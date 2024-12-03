cc2ca5d1f5e5d094862eaba809c3b206439816f2
Elaine <yylam43-c@my.cityu.edu.hk>
2024-12-03 16:56:36 +0800

    update for the evaluation

--- nn/ultra.py
+++ nn/ultra.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     model = YOLO("yolo11n-cls.pt")  # load a pretrained model (recommended for training)
 
     # Train the model
-    results = model.train(data="AIGC-Detection-Dataset\AIGC-Detection-Dataset", epochs=50, imgsz=224)
+    results = model.train(data="AIGC-Detection-Dataset\AIGC-Detection-Dataset", epochs=40, imgsz=224, project='', dropout=0.3)  # train the model for 50 epochs
 
-    model.save("fine.pt")  # save the model to file
+    # model.save("fine.pt")  # save the model to file
     model._save_to_state_dict("fine_state_dict.pt")  # save the model to state_dict
